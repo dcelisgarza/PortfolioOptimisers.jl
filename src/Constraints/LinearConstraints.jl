8e6dbdb6a8bf75bc5c4f9ab221dc79ebc1f6f8ea
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2025-03-05 18:44:20 +0000

    Revert "Consistent names."
    This reverts commit 4b3b4e4411b84e40aaeb2e83562b77f51c887126.

--- src/Constraints/LinearConstraints.jl
+++ src/Constraints/LinearConstraints.jl
@@ -15,49 +15,49 @@ function LinearConstraint(; lhs::LinearConstraintAtom, rhs::LinearConstraintAtom
                                                                                   comp,
                                                                                   kind)
 end
-function get_asset_constraint_data(lca::LinearConstraintAtom{<:AbstractVector,
-                                                             <:AbstractVector,
-                                                             <:AbstractVector, <:Real},
+function get_asset_constraint_data(hs::LinearConstraintAtom{<:AbstractVector,
+                                                            <:AbstractVector,
+                                                            <:AbstractVector, <:Real},
                                    asset_sets::DataFrame; strict::Bool = false)
     group_names = names(asset_sets)
     N = nrow(asset_sets)
-    A = Vector{promote_type(eltype(lca.coef), typeof(lca.cnst))}(undef, 0)
-    sizehint!(A, length(lca.group))
-    for (group, name, coef) ∈ zip(lca.group, lca.name, lca.coef)
+    A = Vector{promote_type(eltype(hs.coef), typeof(hs.cnst))}(undef, 0)
+    sizehint!(A, length(hs.group))
+    for (group, name, coef) ∈ zip(hs.group, hs.name, hs.coef)
         if !(isnothing(group) || string(group) ∉ group_names)
             idx = asset_sets[!, group] .== name
             idx = coef * idx
             append!(A, idx)
         elseif strict
-            throw(ArgumentError("$(string(group)) is not in $(group_names).\n$(lca)."))
+            throw(ArgumentError("$(string(group)) is not in $(group_names).\n$(hs)."))
         else
-            @warn("$(string(group)) is not in $(group_names).\n$(lca).")
+            @warn("$(string(group)) is not in $(group_names).\n$(hs).")
         end
     end
-    tcnst = lca.cnst
+    tcnst = hs.cnst
     return if isempty(A)
         A, tcnst
     else
         vec(sum(reshape(A, N, :); dims = 2)), tcnst
     end
 end
-function get_asset_constraint_data(lca::LinearConstraintAtom{<:Any, <:Any, <:Real, <:Real},
+function get_asset_constraint_data(hs::LinearConstraintAtom{<:Any, <:Any, <:Real, <:Real},
                                    asset_sets::DataFrame; strict::Bool = false)
     group_names = names(asset_sets)
     N = nrow(asset_sets)
-    A = Vector{promote_type(eltype(lca.coef), typeof(lca.cnst))}(undef, 0)
+    A = Vector{promote_type(eltype(hs.coef), typeof(hs.cnst))}(undef, 0)
     sizehint!(A, N)
-    (; group, name, coef) = lca
+    (; group, name, coef) = hs
     if !(isnothing(group) || string(group) ∉ group_names)
         idx = asset_sets[!, group] .== name
         idx = coef * idx
         append!(A, idx)
     elseif strict
-        throw(ArgumentError("$(string(group)) is not in $(group_names).\n$(lca)"))
+        throw(ArgumentError("$(string(group)) is not in $(group_names)."))
     else
-        @warn("$(string(group)) is not in $(group_names).\n$(lca)")
+        @warn("$(string(group)) is not in $(group_names).")
     end
-    tcnst = lca.cnst
+    tcnst = hs.cnst
     return A, tcnst
 end
 function relative_factor_constraint_sign(::AssetLinearConstraint)
