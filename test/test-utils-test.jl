8057b619a5f2e62c8199d1b2f71d2fdc32ff2ee0
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2025-03-22 15:14:57 +0000

    Fix broken test.

--- test/test-utils-test.jl
+++ test/test-utils-test.jl
@@ -16,21 +16,25 @@
         dfy[!, :date] = (today() - Day(100)):Day(1):today()
         Py = TimeArray(dfy; timestamp = :date)
 
-        X1, F1 = prices_to_returns(Px, Py; missing_col_percent = 0.1,
-                                   missing_row_percent = 0.5)
-        X2, F2 = prices_to_returns(Px, Py; missing_col_percent = 0.1,
-                                   missing_row_percent = Inf)
+        ts1, X1, F1 = prices_to_returns(Px, Py; missing_col_percent = 0.1,
+                                        missing_row_percent = 0.5)
+        ts2, X2, F2 = prices_to_returns(Px, Py; missing_col_percent = 0.1,
+                                        missing_row_percent = Inf)
 
         df = CSV.read(joinpath(@__DIR__, "assets/prices_to_returns_X_F.csv"), DataFrame)
         @test hcat(vcat(X1, X2), vcat(F1, F2)) == df
 
-        X3, F3 = prices_to_returns(Px; missing_col_percent = 0.1, missing_row_percent = 0.5)
+        ts3, X3, F3 = prices_to_returns(Px; missing_col_percent = 0.1,
+                                        missing_row_percent = 0.5)
         @test isempty(F3)
 
-        X4, F4 = prices_to_returns(Px; missing_col_percent = 0.1, missing_row_percent = Inf)
+        ts4, X4, F4 = prices_to_returns(Px; missing_col_percent = 0.1,
+                                        missing_row_percent = Inf)
         @test isempty(F4)
 
         df = CSV.read(joinpath(@__DIR__, "assets/prices_to_returns_X.csv"), DataFrame)
         @test vcat(X3, X4) == df
+
+        @test dfy[2:end, :date] == ts1[!, 1] == ts2[!, 1] == ts3[!, 1] == ts4[!, 1]
     end
 end
