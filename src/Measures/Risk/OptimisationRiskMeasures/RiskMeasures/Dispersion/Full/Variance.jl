58449186b1403f2ac24f000b95969ef8e6acfdb3
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2025-03-23 16:08:10 +0000

    Fix variance factory.

--- src/Measures/Risk/OptimisationRiskMeasures/RiskMeasures/Dispersion/Full/Variance.jl
+++ src/Measures/Risk/OptimisationRiskMeasures/RiskMeasures/Dispersion/Full/Variance.jl
@@ -36,7 +36,7 @@ function (r::Variance)(w::AbstractVector)
     return dot(w, r.sigma, w)
 end
 function risk_measure_factory(r::Variance; prior::AbstractPriorModel)
-    sigma = risk_measure_nothing_matrix_factory(r.sigma, prior.skt)
+    sigma = risk_measure_nothing_matrix_factory(r.sigma, prior.sigma)
     return Variance(; settings = r.settings, formulation = r.formulation, sigma = sigma,
                     a_rc = r.a_rc, b_rc = r.b_rc)
 end
