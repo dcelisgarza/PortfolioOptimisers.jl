5615d9e1d03c390c5870a72d382bef3340744152
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2025-03-26 18:58:42 +0000

    Both scalar and vector rms work.

--- src/Optimisation/Clustering/Hierarchical/HierarchicalEqualRiskContribution.jl
+++ src/Optimisation/Clustering/Hierarchical/HierarchicalEqualRiskContribution.jl
@@ -22,13 +22,9 @@ function HierarchicalEqualRiskContribution(;
     return HierarchicalEqualRiskContribution{typeof(opt), typeof(ri), typeof(ro)}(opt, ri,
                                                                                   ro)
 end
-function optimise!(hc::HierarchicalEqualRiskContribution{<:Any, <:OptimisationRiskMeasure,
+function herc_risk(hc::HierarchicalEqualRiskContribution{<:Any, <:OptimisationRiskMeasure,
                                                          <:OptimisationRiskMeasure},
-                   rd::ReturnsData = ReturnsData(); strict::Bool = false)
-    pm = prior(hc.opt.pe, rd.X, rd.F)
-    clm = clusterise(hc.opt.cle, pm.X)
-    idx = cutree(clm.clustering; k = clm.k)
-    cls = [findall(x -> x == i, idx) for i ∈ 1:(clm.k)]
+                   pm::AbstractPriorModel, cls::AbstractVector)
     ri = risk_measure_factory(hc.ri; prior = pm, solvers = hc.opt.slv)
     riku = unitary_expected_risks(ri, pm.X, hc.opt.fees)
     if hc.ri === hc.ro
@@ -39,7 +35,7 @@ function optimise!(hc::HierarchicalEqualRiskContribution{<:Any, <:OptimisationRi
         roku = unitary_expected_risks(ro, pm.X, hc.opt.fees)
     end
     rkbo = zeros(eltype(pm.X), size(pm.X, 2))
-    rkcl = Vector{eltype(pm.X)}(undef, clm.k)
+    rkcl = Vector{eltype(pm.X)}(undef, length(cls))
     w = Vector{eltype(pm.X)}(undef, size(pm.X, 2))
     for (i, cl) ∈ pairs(cls)
         rkbo[cl] .= inv.(view(roku, cl))
@@ -49,54 +45,8 @@ function optimise!(hc::HierarchicalEqualRiskContribution{<:Any, <:OptimisationRi
         w[cl] ./= sum(view(w, cl))
         rkbo[cl] .= zero(eltype(pm.X))
     end
-    nd = to_tree(clm.clustering)[2]
-    hs = [i.height for i ∈ nd]
-    nd = nd[sortperm(hs; rev = true)]
-    # Treat each cluster as its own portfolio and optimise each one individually.
-    # Calculate the weight of each cluster relative to the other clusters.
-    for i ∈ nd[1:(clm.k - 1)]
-        if is_leaf(i)
-            continue
-        end
-        # Do this recursively accounting for the dendrogram structure.
-        ln = pre_order(i.left)
-        rn = pre_order(i.right)
-        lc = Int[]
-        rc = Int[]
-        for (i, cl) ∈ pairs(cls)
-            if issubset(cl, ln)
-                push!(lc, i)
-            elseif issubset(cl, rn)
-                push!(rc, i)
-            end
-        end
-        lrisk = sum(view(rkcl, lc))
-        rrisk = sum(view(rkcl, rc))
-        risk = lrisk + rrisk
-        # Allocate weight to clusters.
-        alpha = one(lrisk) - lrisk / risk
-        # This implicitly multiplies the asset risks by the cluster risk. We eliminate the allocation of a vector of cluster weights, and a loop at the end.
-        # wcl = ones(eltype(pm.X), clm.k)
-        # for i ∈ nd[1:(clm.k - 1)]
-        #     ...
-        #     <this loop>
-        #     ...
-        #     wcl[lc] *= alpha
-        #     wcl[rc] *= one(alpha) - alpha
-        # end
-        # for (i, cl) ∈ pairs(cls)
-        #     w[cl] *= view(rkcl, i)
-        # end
-        # because `ln` and `rn` contain `cl`.
-        w[ln] *= alpha
-        w[rn] *= one(alpha) - alpha
-    end
-    return finalise_hierarchical_weights(hc.opt.cwf,
-                                         weight_bounds_constraints(hc.opt.wb; N = length(w),
-                                                                   strict = strict),
-                                         w / sum(w))
+    return w, rkcl
 end
-
 function herc_scalarised_risk_o!(::SumScalariser, wk::AbstractVector, riro::Bool,
                                  roku::AbstractVector, rkbo::AbstractVector,
                                  cl::AbstractVector,
@@ -113,8 +63,8 @@ function herc_scalarised_risk_o!(::SumScalariser, wk::AbstractVector, riro::Bool
     end
     return crisk
 end
-function herc_scalarised_risk_i!(sce::SumScalariser, wk::AbstractVector,
-                                 riku::AbstractVector, cl::AbstractVector,
+function herc_scalarised_risk_i!(::SumScalariser, wk::AbstractVector, riku::AbstractVector,
+                                 cl::AbstractVector,
                                  ris::AbstractVector{<:OptimisationRiskMeasure},
                                  X::AbstractMatrix, fees::Union{Nothing, <:Fees})
     risk = zeros(eltype(X), length(cl), 2)
@@ -126,14 +76,10 @@ function herc_scalarised_risk_i!(sce::SumScalariser, wk::AbstractVector,
     end
     return view(risk, :, 2)
 end
-function optimise!(hc::HierarchicalEqualRiskContribution{<:Any,
+function herc_risk(hc::HierarchicalEqualRiskContribution{<:Any,
                                                          <:AbstractVector{<:OptimisationRiskMeasure},
                                                          <:AbstractVector{<:OptimisationRiskMeasure}},
-                   rd::ReturnsData = ReturnsData(); strict::Bool = false)
-    pm = prior(hc.opt.pe, rd.X, rd.F)
-    clm = clusterise(hc.opt.cle, pm.X)
-    idx = cutree(clm.clustering; k = clm.k)
-    cls = [findall(x -> x == i, idx) for i ∈ 1:(clm.k)]
+                   pm::AbstractPriorModel, cls::AbstractVector)
     ri = risk_measure_factory(hc.ri; prior = pm, solvers = hc.opt.slv)
     riro = false
     riku = Vector{eltype(pm.X)}(undef, size(pm.X, 2))
@@ -146,7 +92,7 @@ function optimise!(hc::HierarchicalEqualRiskContribution{<:Any,
         roku = Vector{eltype(pm.X)}(undef, size(pm.X, 2))
     end
     rkbo = zeros(eltype(pm.X), size(pm.X, 2))
-    rkcl = Vector{eltype(pm.X)}(undef, clm.k)
+    rkcl = Vector{eltype(pm.X)}(undef, length(cls))
     w = Vector{eltype(pm.X)}(undef, size(pm.X, 2))
     wk = zeros(eltype(pm.X), size(pm.X, 2))
     for (i, cl) ∈ pairs(cls)
@@ -155,6 +101,15 @@ function optimise!(hc::HierarchicalEqualRiskContribution{<:Any,
                                           hc.opt.fees)
         rkbo[cl] .= zero(eltype(pm.X))
     end
+    return w, rkcl
+end
+function optimise!(hc::HierarchicalEqualRiskContribution, rd::ReturnsData = ReturnsData();
+                   strict::Bool = false)
+    pm = prior(hc.opt.pe, rd.X, rd.F)
+    clm = clusterise(hc.opt.cle, pm.X)
+    idx = cutree(clm.clustering; k = clm.k)
+    cls = [findall(x -> x == i, idx) for i ∈ 1:(clm.k)]
+    w, rkcl = herc_risk(hc, pm, cls)
     nd = to_tree(clm.clustering)[2]
     hs = [i.height for i ∈ nd]
     nd = nd[sortperm(hs; rev = true)]
