dba98daa7f88c461c0d363e7050cfb28fc760eac
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2025-04-12 23:38:05 +0100

    Update test-equation-parsing-test.jl

--- test/test-equation-parsing-test.jl
+++ test/test-equation-parsing-test.jl
@@ -1,4 +1,4 @@
-@safetestsets "Equation parsing tests" begin
+@safetestset "Equation parsing tests" begin
     using PortfolioOptimisers
     eqn = "a - 1*b - c*2 + 3*d/4 - 1/5*e*6 + - 7*f*8/9 + 10/11*g*12 + 13/14*h*15/16 - 3*a +--6 --- 6 = -5"
     res = parse_constraint_equation(eqn)
