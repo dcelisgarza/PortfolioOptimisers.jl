edb5a8ea91f5e6c6761daa6b674be33adc35a356
dcelisgarza <daniel.celis.garza@gmail.com>
2025-06-30 13:02:21 +0100

    Fix broken tests.

--- test/test-1near-optimal-centering-test.jl
+++ test/test-1near-optimal-centering-test.jl
@@ -25,7 +25,7 @@
         for rtol in [1e-6, 5e-6, 1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1, 5e-1]
             res = isapprox(a, b; rtol = rtol)
             if res
-                return t
+                return rtol
             end
         end
         return 0.0
