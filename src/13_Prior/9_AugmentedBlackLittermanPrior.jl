9c9450af86a39a1a5fe47e09a742519cf574c75d
dcelisgarza <daniel.celis.garza@gmail.com>
2025-10-03 19:01:33 +0100

    Fix abstract type of augmented bl prior.

--- src/13_Prior/9_AugmentedBlackLittermanPrior.jl
+++ src/13_Prior/9_AugmentedBlackLittermanPrior.jl
@@ -1,5 +1,5 @@
 struct AugmentedBlackLittermanPrior{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
-                                    T14, T15} <: AbstractLowOrderPriorEstimator_2_1
+                                    T14, T15} <: AbstractLowOrderPriorEstimator_2_2
     a_pe::T1
     f_pe::T2
     mp::T3
