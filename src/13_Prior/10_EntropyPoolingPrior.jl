9822f7c2048c8a38f0ae5f2cb5c1d94120278963
dcelisgarza <daniel.celis.garza@gmail.com>
2025-10-10 01:07:20 +0100

    Opinion pooling docs.

--- src/13_Prior/10_EntropyPoolingPrior.jl
+++ src/13_Prior/10_EntropyPoolingPrior.jl
@@ -589,7 +589,11 @@ struct EntropyPoolingPrior{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T1
                                  alg::AbstractEntropyPoolingAlgorithm)
         if isa(w, ProbabilityWeights)
             @argcheck(!isempty(w))
-            normalize!(w, 1)
+            if ismutable(w)
+                normalize!(w, 1)
+            else
+                w = normalize(w, 1)
+            end
         end
         if !isnothing(mu_views) ||
            !isnothing(var_views) ||
