3bb38216d8bf33b756575f43c6c8eecaed8d6fec
dcelisgarza <daniel.celis.garza@gmail.com>
2025-11-06 18:58:08 +0000

    Return to the previous state.

--- docs/make.jl
+++ docs/make.jl
@@ -45,18 +45,18 @@ code_files = filter(x -> endswith(x, ".jl"), files)
 data_files = filter(x -> (endswith(x, ".csv") || endswith(x, ".csv.gz")), files)
 example_pages = fix_suffix_md.("examples/" .* code_files)
 
-# for file in data_files
-#     cp(joinpath(@__DIR__, "../examples/" * file),
-#        joinpath(@__DIR__, "src/examples/" * file); force = true)
-# end
+for file in data_files
+    cp(joinpath(@__DIR__, "../examples/" * file),
+       joinpath(@__DIR__, "src/examples/" * file); force = true)
+end
 
-# for file in code_files
-#     Literate.markdown(example_path * file, build_path_md;
-#                       preprocess = pre_process_content_md, postprocess = postprocess,
-#                       documenter = true, credit = true)
-#     Literate.notebook(example_path * file, example_path;
-#                       preprocess = pre_process_content_nb, documenter = true, credit = true)
-# end
+for file in code_files
+    Literate.markdown(example_path * file, build_path_md;
+                      preprocess = pre_process_content_md, postprocess = postprocess,
+                      documenter = true, credit = true)
+    Literate.notebook(example_path * file, example_path;
+                      preprocess = pre_process_content_nb, documenter = true, credit = true)
+end
 
 root_pages = [file
               for file in readdir(joinpath(@__DIR__, "src")) if splitext(file)[2] == ".md"]
@@ -72,31 +72,28 @@ makedocs(; #modules = [PortfolioOptimisers],
          sitename = "PortfolioOptimisers.jl",
          format = DocumenterVitepress.MarkdownVitepress(;
                                                         repo = "https://github.com/dcelisgarza/PortfolioOptimisers.jl"),
-         pages = [
-                  # "Home" => root_pages[1];
-                  # "Examples" => example_pages;
-                  "API" => [joinpath.(api_pages[1][1][idx1:end], api_pages[1][3][1]);
-                            # "Moments" => joinpath.(api_pages[2][1][idx1:end],
-                            #                        api_pages[2][3])
-                            # "Distance" => joinpath.(api_pages[3][1][idx1:end],
-                            #                         api_pages[3][3])
-                            # "Phylogeny" => joinpath.(api_pages[4][1][idx1:end],
-                            #                          api_pages[4][3])
-                            # "Constraint Generation" => joinpath.(api_pages[5][1][idx1:end],
-                            #                                      api_pages[5][3])
-                            # "Prior" => joinpath.(api_pages[6][1][idx1:end], api_pages[6][3]);
-                            # "Uncertainty Sets" => joinpath.(api_pages[7][1][idx1:end],
-                            #                                 api_pages[7][3])
-                            # "Risk Measures" => joinpath.(api_pages[8][1][idx1:end],
-                            #                              api_pages[8][3])
-                            # "Optimisation" => [joinpath.(api_pages[9][1][idx1:end],
-                            #                              api_pages[9][3])
-                            #                    "Constraints" => joinpath.(api_pages[10][1][idx1:end],
-                            #                                               api_pages[10][3])
-                            #                    "Risk Constraints" => joinpath.(api_pages[11][1][idx1:end],
-                            #                                                    api_pages[11][3])]
-
-                            ];
+         pages = ["Home" => root_pages[1];
+                  "Examples" => example_pages;
+                  "API" => [joinpath.(api_pages[1][1][idx1:end], api_pages[1][3]);
+                            "Moments" => joinpath.(api_pages[2][1][idx1:end],
+                                                   api_pages[2][3])
+                            "Distance" => joinpath.(api_pages[3][1][idx1:end],
+                                                    api_pages[3][3])
+                            "Phylogeny" => joinpath.(api_pages[4][1][idx1:end],
+                                                     api_pages[4][3])
+                            "Constraint Generation" => joinpath.(api_pages[5][1][idx1:end],
+                                                                 api_pages[5][3])
+                            "Prior" => joinpath.(api_pages[6][1][idx1:end], api_pages[6][3]);
+                            "Uncertainty Sets" => joinpath.(api_pages[7][1][idx1:end],
+                                                            api_pages[7][3])
+                            "Risk Measures" => joinpath.(api_pages[8][1][idx1:end],
+                                                         api_pages[8][3])
+                            "Optimisation" => [joinpath.(api_pages[9][1][idx1:end],
+                                                         api_pages[9][3])
+                                               "Constraints" => joinpath.(api_pages[10][1][idx1:end],
+                                                                          api_pages[10][3])
+                                               "Risk Constraints" => joinpath.(api_pages[11][1][idx1:end],
+                                                                               api_pages[11][3])]];
                   "Contribute" => contribute;
                   "References" => root_pages[2]],
          plugins = [CitationBibliography(joinpath(@__DIR__, "src", "References.bib");
