ffefb29ece1640956eb56a72acb46d921d40ed30
dcelisgarza <dcelisgarza@users.noreply.github.com>
2025-11-16 15:47:46 +0000

    Better type stable api with defaults.

--- src/19_RiskMeasures/02_Variance.jl
+++ src/19_RiskMeasures/02_Variance.jl
@@ -619,25 +619,31 @@ end
 function (r::UncertaintySetVariance)(w::VecNum)
     return dot(w, r.sigma, w)
 end
-function no_bounds_risk_measure(r::UncertaintySetVariance, ::Union{Val{true}, Nothing})
+function no_bounds_risk_measure(r::UncertaintySetVariance, flag::Union{Val{false}, Val{true}, Nothing} = nothing)
+    return _no_bounds_risk_measure(r, flag)
+end
+function _no_bounds_risk_measure(r::UncertaintySetVariance, ::Union{Val{true}, Nothing})
     return UncertaintySetVariance(;
                                   settings = RiskMeasureSettings(; rke = r.settings.rke,
                                                                  scale = r.settings.scale),
                                   r.ucs, sigma = r.sigma)
 end
-function no_bounds_risk_measure(r::UncertaintySetVariance, ::Val{false})
+function _no_bounds_risk_measure(r::UncertaintySetVariance, ::Val{false})
     return Variance(;
                     settings = RiskMeasureSettings(; rke = r.settings.rke,
                                                    scale = r.settings.scale), rc = nothing,
                     sigma = r.sigma)
 end
-function no_bounds_no_risk_expr_risk_measure(r::UncertaintySetVariance, ::Val{true})
+function no_bounds_no_risk_expr_risk_measure(r::UncertaintySetVariance, flag::Union{Val{false}, Val{true}, Nothing} = nothing)
+    return _no_bounds_no_risk_expr_risk_measure(r, flag)
+end
+function _no_bounds_no_risk_expr_risk_measure(r::UncertaintySetVariance, ::Union{Val{true}, Nothing})
     return UncertaintySetVariance(;
                                   settings = RiskMeasureSettings(; rke = false,
                                                                  scale = r.settings.scale),
                                   r.ucs, sigma = r.sigma)
 end
-function no_bounds_no_risk_expr_risk_measure(r::UncertaintySetVariance, ::Val{false})
+function _no_bounds_no_risk_expr_risk_measure(r::UncertaintySetVariance, ::Val{false})
     return Variance(;
                     settings = RiskMeasureSettings(; rke = false, scale = r.settings.scale),
                     rc = nothing, sigma = r.sigma)
