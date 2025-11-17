5c0921df738557031d572fbf8619ce25b4ef78f5
dcelisgarza <daniel.celis.garza@gmail.com>
2025-11-17 13:40:33 +0000

    Add no_bounds_no_risk_expr_risk_measure function to risk tracking risk measure so it doesn't double count.

--- test/test_16_mean_risk_optimisation.jl
+++ test/test_16_mean_risk_optimisation.jl
@@ -263,8 +263,7 @@
         opt = JuMPOptimiser(; pe = pr, slv = slv)
         i = 1
         for r in rs
-            r1 = RiskTrackingRiskMeasure(; tr = tr,
-                                         r = no_bounds_no_risk_expr_risk_measure(r),
+            r1 = RiskTrackingRiskMeasure(; tr = tr, r = r,
                                          alg = DependentVariableTracking())
             mr = MeanRisk(; r = r1, obj = MaximumRatio(; rf = rf), opt = opt)
             res = optimise(mr, rd)
@@ -306,8 +305,7 @@
         opt = JuMPOptimiser(; pe = pr, slv = slv)
         i = 1
         for r in rs
-            r1 = RiskTrackingRiskMeasure(; tr = tr,
-                                         r = no_bounds_no_risk_expr_risk_measure(r),
+            r1 = RiskTrackingRiskMeasure(; tr = tr, r = r,
                                          alg = IndependentVariableTracking())
             mr = MeanRisk(; r = r1, obj = MaximumRatio(; rf = rf), opt = opt)
             res = optimise(mr, rd)
