ed6f43381275b82c3a5816f7f93c6702cf98991d
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2025-12-19 10:27:57 +0000

    Fix broken docs test

--- src/19_RiskMeasures/19_PowerXatRisk.jl
+++ src/19_RiskMeasures/19_PowerXatRisk.jl
@@ -1,3 +1,4 @@
+# https://github.com/oxfordcontrol/Clarabel.jl/blob/4915b83e0d900d978681d5e8f3a3a5b8e18086f0/warmstart_test/portfolioOpt/higherorderRiskMeansure.jl#L23
 struct PowerValueatRisk <: RiskMeasure end
 struct PowerValueatRiskRange <: RiskMeasure end
 struct PowerDrawdownatRisk <: RiskMeasure end
