83ea75004fd66b3256e781faab832d8c991e9b47
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2026-01-09 00:17:59 +0000

    Fix doctests

--- src/11_Phylogeny/02_Clusters.jl
+++ src/11_Phylogeny/02_Clusters.jl
@@ -396,7 +396,7 @@ ClustersEstimator
       │      │    me ┼ SimpleExpectedReturns
       │      │       │   w ┴ nothing
       │      │    ce ┼ GeneralCovariance
-      │      │       │   ce ┼ SimpleCovariance: SimpleCovariance(true)
+      │      │       │   ce ┼ StatsBase.SimpleCovariance: StatsBase.SimpleCovariance(true)
       │      │       │    w ┴ nothing
       │      │   alg ┴ Full()
       │   mp ┼ DenoiseDetoneAlgMatrixProcessing
