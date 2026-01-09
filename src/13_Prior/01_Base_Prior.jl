54c64a8f2b9ec9f2dba6ce5f62fcf1d4e1891f74
dcelisgarza <daniel.celis.garza@gmail.com>
2026-01-09 18:04:52 +0000

    Increase coverage.

--- src/13_Prior/01_Base_Prior.jl
+++ src/13_Prior/01_Base_Prior.jl
@@ -347,7 +347,7 @@ Compute the weighted average centrality for a network or phylogeny result.
 """
 function average_centrality(ne::NwE_Ph_ClE_Cl, cent::AbstractCentralityAlgorithm, w::VecNum,
                             pr::AbstractPriorResult; kwargs...)
-    return LinearAlgebra.dot(centrality_vector(ne, cent, pr.X; kwargs...).X, w)
+    return LinearAlgebra.dot(centrality_vector(ne, cent, pr; kwargs...).X, w)
 end
 """
     average_centrality(cte::CentralityEstimator, w::VecNum, pr::AbstractPriorResult;
