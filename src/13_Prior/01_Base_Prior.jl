0dd70333d8150f14cfc3bff72d3dbebe3c18887f
dcelisgarza <daniel.celis.garza@gmail.com>
2026-03-17 12:44:09 +0000

    simplify function signatures

--- src/13_Prior/01_Base_Prior.jl
+++ src/13_Prior/01_Base_Prior.jl
@@ -274,17 +274,12 @@ function phylogeny_matrix(pl::NwE_ClE_Cl, pr::Pr_RR; rd::Option{<:ReturnsResult}
     X = isnothing(rd) || cle_pr ? pr.X : rd.X
     return phylogeny_matrix(pl, X; kwargs...)
 end
-function phylogeny_constraints(plc::PlCE_PlC, pr::Pr_RR;
+function phylogeny_constraints(plc::AbstractPhylogenyConstraintEstimator, pr::Pr_RR;
                                rd::Option{<:ReturnsResult} = nothing, cle_pr::Bool = true,
                                kwargs...)
     X = isnothing(rd) || cle_pr ? pr.X : pr.rd.X
     return phylogeny_constraints(plc, X; kwargs...)
 end
-#! Start: Only exists to avoid ambiguities.
-function phylogeny_constraints(plc::AbstractPhylogenyConstraintResult, ::Pr_RR; kwargs...)
-    return plc
-end
-#! End: Only exists to avoid ambiguities.
 """
     centrality_vector(cte::CentralityEstimator, pr::AbstractPriorResult; kwargs...)
 
