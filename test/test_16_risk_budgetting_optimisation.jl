44152fcdaf7fb92df44763ebcddc6418ba457377
dcelisgarza <daniel.celis.garza@gmail.com>
2026-04-02 19:21:01 +0100

    Relax tolerances

--- test/test_16_risk_budgetting_optimisation.jl
+++ test/test_16_risk_budgetting_optimisation.jl
@@ -476,7 +476,7 @@
                         0.06254237111913212, 0.053763355951943745, 0.08026745642402928,
                         0.03515784043192167, 0.06306076653811903, 0.05566770018572234,
                         0.06352751462163037, 0.029346131719786624, 0.05375126385057467,
-                        0.06560682427264843, 0.05108403002238485], rtol = 1e-6)
+                        0.06560682427264843, 0.05108403002238485], rtol = 5e-5)
 
         rb = RiskBudgeting(; r = r, opt = opt,
                            rba = AssetRiskBudgeting(; alg = MixedIntegerRiskBudgeting(),
@@ -485,7 +485,7 @@
         rkc = risk_contribution(r, res.w, pr.X)
         v1, m1 = findmin(rkc)
         v2, m2 = findmax(rkc)
-        success = isapprox(v2 / v1, 20; rtol = 5e-5)
+        success = isapprox(v2 / v1, 20; rtol = 1e-4)
         if !success
             find_tol(v2 / v1, 20)
         end
@@ -499,7 +499,7 @@
                         0.05607985937522164, 0.0523858066047695, 0.08241709539390478,
                         0.04482404375427151, 0.07812672108647016, 0.07296166947749294,
                         0.08910512294473034, 0.04281741172335355, 0.0840529450637892,
-                        0.10412048295611062, 0.08726672366225435], rtol = 1e-6)
+                        0.10412048295611062, 0.08726672366225435], rtol = 5e-5)
 
         rd2 = PortfolioOptimisers.returns_result_view(rd, 1:2:20)
         pr2 = prior(EmpiricalPrior(), rd2)
@@ -513,16 +513,12 @@
         rkc = risk_contribution(r, res.w, pr2.X)
         v1 = minimum(rkc)
         v2 = maximum(rkc)
-        success = isapprox(v2 / v1, 1; rtol = 5e-4)
-        if !success
-            find_tol(v2 / v1, 1)
-        end
-        @test success
+        @test isapprox(v2 / v1, 1; rtol = 5e-4)
         @test isapprox(res.w,
                        [-11.9670204785192, -20.27855656466867, -9.031638570761155,
                         -12.404198792206339, 23.38560846510856, -11.411103386313773,
                         15.532421847999915, 11.074304614043934, 5.235656319779921,
-                        10.963933763939947], rtol = 1e-6)
+                        10.963933763939947], rtol = 5e-4)
 
         rb = RiskBudgeting(; r = r, opt = opt,
                            rba = AssetRiskBudgeting(; alg = MixedIntegerRiskBudgeting(),
@@ -531,17 +527,13 @@
         rkc = risk_contribution(r, res.w, pr2.X)
         v1, m1 = findmin(rkc)
         v2, m2 = findmax(rkc)
-        success = isapprox(v2 / v1, 10; rtol = 1e-4)
-        if !success
-            find_tol(v2 / v1, 10)
-        end
-        @test success
+        @test isapprox(v2 / v1, 10; rtol = 5e-4)
         @test m1 == 1
         @test m2 == 10
         @test isapprox(res.w,
                        [-2.4295346851425466, -4.782006626613654, -2.4181860407479356,
                         -3.5475446827000723, 5.25869258317521, -3.557616841873538,
                         3.737346772646009, 3.54749774747547, 1.626855992937381,
-                        3.665078207675533], rtol = 1e-6)
+                        3.665078207675533], rtol = 5e-4)
     end
 end
