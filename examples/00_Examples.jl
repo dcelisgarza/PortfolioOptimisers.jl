4e0d287a446d6be34dd799c8e36238030c98544d
dcelisgarza <daniel.celis.garza@gmail.com>
2026-06-19 20:25:30 +0100

    Fix link

--- examples/00_Examples.jl
+++ examples/00_Examples.jl
@@ -3,5 +3,5 @@
 
 The examples provided in this package demonstrate how to use the various features of `PortfolioOptimisers.jl`. Each example is designed to illustrate a specific feature or use case, ranging from basic portfolio optimization techniques to more advanced functionality.
 
-For a quick introduction of the package's functionality, see the [User Guide](https://dcelisgarza.github.io/PortfolioOptimisers.jl/stable/user_guide/00_User_Guide).
+For a quick introduction of the package's functionality, see the [User Guide](https://dcelisgarza.github.io/PortfolioOptimisers.jl/stable/user_guide/00_Introduction).
 =#
